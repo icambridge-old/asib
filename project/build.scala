7b93d0a56d0f59b0d222da0d6c2b5bb564b08346
Iain Cambridge <github@icambridge.me>
2012-04-04 09:04:54 +0200

    Update project/build.scala

--- project/build.scala
+++ project/build.scala
@@ -2,17 +2,10 @@ import sbt._
 object MyApp extends Build
 {
 	lazy val root =
-<<<<<<< HEAD
-		Project("", file(".")) dependsOn(dispatchLiftJson) dependsOn(xsbtStart)
-	lazy val dispatchLiftJson =
-		uri("git://github.com/dispatch/dispatch-lift-json")
-	lazy val xsbtStart =
-		uri("git://github.com/typesafehub/xsbt-start-script-plugin.git")
-=======
+
 		Project("", file(".")) dependsOn(dispatchLiftJson) dependsOn(redis)
 	lazy val dispatchLiftJson =
 		uri("git://github.com/dispatch/dispatch-lift-json")
 	lazy val redis = uri("git://github.com/icambridge/scala-redis.git")
 
->>>>>>> 3366d8a4aaf0adcf885195ec334ba86d7af34cc7
 }
